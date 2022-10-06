1b7ec187e4a3bce2392e25de36f876c75c80395c
hopeVaughn <hopevaughnwarren@gmail.com>
2022-10-06 13:25:07 -0700

    add superheros to mock data cause it makes Jackie laugh

---
+++ mockData/mockData.js
@@ -0,0 +1,21 @@
+
+const mockData = [
+  {
+    "name": "Molecule Man",
+    "age": 29,
+    "secretIdentity": "Dan Jukes",
+    "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+  },
+  {
+    "name": "Madame Uppercut",
+    "age": 39,
+    "secretIdentity": "Jane Wilson",
+    "powers": [
+      "Million tonne punch",
+      "Damage resistance",
+      "Superhuman reflexes"
+    ]
+  }
+]
+
+export default mockData;
